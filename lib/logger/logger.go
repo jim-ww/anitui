edd4056943346c89089340b83c848c7733039db3
jim-ww <jim.w2610@proton.me>
2025-07-15 04:15:25 +0300

    add helper func to set logging lvl

---
+++ lib/logger/logger.go
@@ -0,0 +1,18 @@
+package logger
+
+import "log/slog"
+
+func ParseLogLevel(level string) slog.Level {
+	switch level {
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return slog.LevelDebug
+	}
+}
